c490f4e18b43f7f1d2fe0e486d01b97b28d9bf0a
nmcdorman <nmcdorman@gmail.com>
2020-05-25 20:23:50 -0400

    Add project files.

---
+++ CS Advanced Topics - ExtensionMethods/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CS_Advanced_Topics___ExtensionMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string post = "This is supposed to be a very long blog post blah blah blah...";
+            var shortenedPost = post.Shorten(5);
+
+            IEnumerable<int> numbers = new List<int>() { 1, 5, 3, 10, 2, 18 };
+            var max = numbers.Max();
+
+            //Console.WriteLine(shortenedPost);
+            Console.WriteLine(max);
+        }
+    }
+}
---
+++ CS Advanced Topics - ExtensionMethods/StringExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CS_Advanced_Topics___ExtensionMethods
+{
+    public static class StringExtensions
+    {
+        public static string Shorten(this String str, int numberOfWords)
+        {
+            if (numberOfWords < 0)
+                throw new ArgumentOutOfRangeException("numberOfWords should be greater than or equal to 0.");
+            if (numberOfWords == 0)
+                return "";
+
+            var words = str.Split(" ");
+
+            if (words.Length <= numberOfWords)
+                return str;
+
+            return string.Join(" ", words.Take(numberOfWords));
+
+        }
+    }
+}
